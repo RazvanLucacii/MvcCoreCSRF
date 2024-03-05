8c0e173fc9e44c80eda01503876d2dab6e7bb99e
RazvanLucacii <rlucaci48@gmail.com>
2024-03-05 13:32:33 +0100

    Add project files.

---
+++ MvcCoreCSRF/Controllers/ManagedController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            if (usuario.ToLower() == "admin"
+                && password.ToLower() == "admin")
+            {
+                //ALMACENAMOS EN SESSION EL USUARIO VALIDADO
+                HttpContext.Session.SetString("USUARIO", usuario);
+                return RedirectToAction("Productos", "Tienda");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("USUARIO");
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCoreCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            //comprobamos si existe el usuario para dejarle entrar o no
+            if(HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Productos(string direccion, string[] producto)
+        {
+            if(HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                //para enviar informacion entre controladores se utiliza TempData["KEY"]
+                TempData["DIRECCION"] = direccion;
+                TempData["PRODUCTOS"] = producto;
+                return RedirectToAction("PedidoFinal");
+            }
+        }
+
+        public IActionResult PedidoFinal()
+        {
+            string[] productos = TempData["PRODUCTOS"] as string[];
+            ViewData["DIRECCION"] = TempData["DIRECCION"].ToString();
+            return View(productos);
+        }
+    }
+}
